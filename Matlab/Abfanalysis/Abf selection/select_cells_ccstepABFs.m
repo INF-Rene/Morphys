6441d1cd6fe61c422ac1320945976d19d3846179
INF-Rene <32510643+INF-Rene@users.noreply.github.com>
2017-10-12 10:52:47 +0200

    select latest recorded abf in case of duplicates

--- Matlab/Abfanalysis/Abf selection/select_cells_ccstepABFs.m
+++ Matlab/Abfanalysis/Abf selection/select_cells_ccstepABFs.m
@@ -9,11 +9,25 @@ ccabf = getccstep(abf);
 
 %find cells that have multiple cc step files
 tt{:,2}=cellfun(@(x) strcat(x ,'.abf'), tt{:,2}, 'UniformOutput', false);
-cells=tt{ismember(tt{:,2},{ccabf.filename}),1};
-[C,ia,ic] = unique(cells);
+cells=tt(ismember(tt{:,2},{ccabf.filename}),:);
+[C,ia,ic] = unique(cells(:,1), 'stable');
 [counts,~]=hist(ic,ia);
 if any(counts>1))
     %determine which abf of the duplicates should be used for analysis
+    dupes={cells{ia(counts>1),1}};
+    for i=1:numel(dupes)
+        a=cells(ismember(cells{:,1},dupes{i}),2:3);
+        idx=find(ismember({ccabf.filename}, a{:,1}));
+        b={ccabf(idx).filetimestart};
+        b=cellfun(@(x) strsplit(x, {':', '-', ' '}), b, 'UniformOutput', false);
+        clear c
+        for j=1:numel(b)
+        c(j)=datetime(cellfun(@str2num, b{j}));
+        end
+        [~,d]=max(c);           %selects the abf that was recorded the latest
+        idx(d)=[];
+        ccabf(idx)=[];
+    end
 end
 
 
